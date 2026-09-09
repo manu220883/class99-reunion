4b307a6856880d65a81d20040d069e9c8be52abe
manu220883 <75961834+manu220883@users.noreply.github.com>
2026-09-09 20:42:29 +0530

    Update Google Apps Script Web App URL
    Updated the Google Apps Script Web App URL in the config.

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-// Paste the deployed Google Apps Script Web App URL here.
-// Example: https://script.google.com/macros/s/XXXXXXXX/exec
 const CONFIG = {
-  APPS_SCRIPT_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyxwEuKLw0MgWIiQMnCQ70ZdWo_OGSwdWcnMApCiS33yJk_xmHV6plNzB40RtblDVme/exec",
   ENABLE_PUBLIC_RESULTS: true
 };
